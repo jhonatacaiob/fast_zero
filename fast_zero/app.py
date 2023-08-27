8fc6802d7df75eb1ea4773b0ad2e3aa831987679
Jhonata Caio <jhonatacaiob@gmail.com>
2023-08-27 17:19:17 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 
-from fast_zero.schemas import UserDB, UserList, UserPublic, UserSchema, Message
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
